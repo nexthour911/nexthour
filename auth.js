3bdb93aae8710fac1e516534043c20dee2f36231
nexthour911 <nextgenn911@gmail.com>
2026-01-02 13:42:28 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,15 +1,18 @@
-[1:29 PM, 1/2/2026] tushar: import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyBNR5Td6W_7TAjfRhoJzYrGJMgy3Ff_E1Q",
+  authDomain: "nexthour-3248f.firebaseapp.com",
+  projectId: "nexthour-3248f",
+  storageBucket: "nexthour-3248f.firebasestorage.app",
+  messagingSenderId: "83247480758",
+  appId: "1:83247480758:web:27f8f8507273d54ee44b17"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
