fd3650dbc1f2d4e60e98faeb5ebd4c0a61911b40
nexthour911 <nextgenn911@gmail.com>
2026-01-02 13:46:14 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,4 @@
+alert("auth.js loaded");
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
